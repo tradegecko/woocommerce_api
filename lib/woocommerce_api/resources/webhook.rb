da119e2525492a1cbf4ffaed8da5226b47ef2214
North <north@tradegecko.com>
2015-04-27 10:58:21 +0800

    Added Webhook resource

---
+++ lib/woocommerce_api/resources/webhook.rb
@@ -0,0 +1,33 @@
+module WoocommerceAPI
+  class Webhook < Resource
+    attribute :id, Integer
+    attribute :name
+    attribute :status
+    attribute :topic
+    attribute :delivery_url
+
+    # Write Only
+    attribute :secret
+
+    # Read Only
+    attribute :created_at, DateTime
+    attribute :updated_at, DateTime
+    attribute :event
+    attribute :hooks, Array
+    attribute :resource
+  end
+end
+
+# Available :topic
+# coupon.created
+# coupon.updated
+# coupon.deleted
+# customer.created
+# customer.updated
+# customer.deleted
+# order.created
+# order.updated
+# order.deleted
+# product.created
+# product.updated
+# product.deleted
