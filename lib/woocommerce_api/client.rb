0ed8051b0efb76f149ca2cbb2dadebffb04f1cda
North <north@tradegecko.com>
2015-04-27 10:47:43 +0800

    Rename base class to client

--- lib/woocommerce_api/client.rb
+++ lib/woocommerce_api/client.rb
@@ -1,10 +1,10 @@
 module WoocommerceAPI
-  class Base
-    extend WoocommerceAPI
+  class Client
     include HTTParty
     format :json
     headers 'Accept' => "application/json"
     headers 'Content-Type' => "application/json"
+    debug_output
 
     def initialize(params={})
       self.class.basic_auth params[:consumer_key], params[:consumer_secret]
