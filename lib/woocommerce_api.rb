5b142a6277f168ed53f43002e8cc8e0494c03c95
North <north@tradegecko.com>
2015-04-27 10:45:11 +0800

    Add and require gems

--- lib/woocommerce_api.rb
+++ lib/woocommerce_api.rb
@@ -1,8 +1,13 @@
 require "rubygems"
+require "active_support"
+require "active_support/core_ext/hash"
+require "active_model"
 require "httparty"
-require 'active_support/inflector'
+require "virtus"
 
-require "woocommerce_api/base"
+require "woocommerce_api/concerns/associations"
+require "woocommerce_api/concerns/attribute_assignment"
+require "woocommerce_api/concerns/singleton"
+require "woocommerce_api/client"
 require "woocommerce_api/resource"
-
-require "woocommerce_api/product"
+Dir["./lib/woocommerce_api/resources/*.rb"].each { |f| require f }
