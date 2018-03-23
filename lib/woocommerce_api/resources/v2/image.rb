27b238cf7af1364815cb9229659369a87d37f001
Philip Mark Deazeta <philip@tradegecko.com>
2018-03-23 12:58:04 +0800

    [BUGFIX] Image cannot be pushed to WC because payload includes ID null

--- lib/woocommerce_api/resources/v2/image.rb
+++ lib/woocommerce_api/resources/v2/image.rb
@@ -1,11 +1,11 @@
 module WoocommerceAPI
   module V2
     class Image < Resource
-      attribute :id, Integer
       attribute :src
       attribute :position, Integer
 
       # Read Only
+      attribute :id, Integer, writer: :private
       attribute :date_created, DateTime, writer: :private
       attribute :date_modified, DateTime, writer: :private
       attribute :title     , String  , writer: :private
